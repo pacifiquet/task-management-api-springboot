bb7d136a05b2d6ae525bb6fc8f1b9819ceefc5b8
Pacifique Twagirayesu <pacifiquetwagirayesu@gmail.com>
2023-08-08 23:01:11 +0800

    resolved failed port in testing

--- src/test/java/com/pacifique/todoapp/MainTest.java
+++ src/test/java/com/pacifique/todoapp/MainTest.java
@@ -1,9 +1,11 @@
 package com.pacifique.todoapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
+@SpringBootTest
 class MainTest {
 
     @Test
